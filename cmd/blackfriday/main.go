174bcd4027c15d13200f677cba1a8552200b1254
NoeGodin <noegodin443@gmail.com>
2025-11-20 15:59:36 +0100

    debug : adding logger to project

--- cmd/blackfriday/main.go
+++ cmd/blackfriday/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	Graphics "AI30_-_BlackFriday/pkg/graphics"
+	"AI30_-_BlackFriday/pkg/logger"
 	Map "AI30_-_BlackFriday/pkg/map"
 	Simulation "AI30_-_BlackFriday/pkg/simulation"
 
@@ -15,6 +16,8 @@ const SCREEN_WIDTH = 700
 const SCREEN_HEIGHT = 700
 
 func main() {
+	logger.InitLogger()
+
 	//NOTE : I guess its for my setup (working on mac) maybe not needed for others idk :/
 	os.Setenv("EBITEN_GRAPHICS_LIBRARY", "opengl")
 
@@ -22,14 +25,20 @@ func main() {
 	ebiten.SetWindowTitle("Black Friday Simulator")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	mapData, err := Map.LoadMapFromFile("maps/store/layout.txt")
+	logger.Info("Loading map...")
+	mapData, err := Map.LoadMapFromFile("maps/store/large_layout.txt")
 	if err != nil {
+		logger.Errorf("Error loading map: %s", err.Error())
 		panic("Error loading map: " + err.Error())
 	}
+
+	logger.Info("Creating simulation...")
 	simu := Simulation.NewSimulation(0, 100.0, mapData)
 
-	// Appel de la fonction
+	logger.Info("Adding agents...")
 	simu.AddClient("agent1")
+
+	logger.Info("Starting game...")
 	game := Graphics.NewGame(SCREEN_WIDTH, SCREEN_HEIGHT, simu)
 	game.Simulation.Run()
 	if err := ebiten.RunGame(game); err != nil {
