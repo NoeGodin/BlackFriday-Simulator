5aba5b1228630a4aaff5b16a19ee8042843b307b
Ouistitive <ouistitive@gmail.com>
2025-12-08 21:35:41 +0100

    add status and cart agents in hud

--- pkg/simulation/agent.go
+++ pkg/simulation/agent.go
@@ -28,7 +28,134 @@ type Agent interface {
 	Move()
 	Speed() float64
 	DryRunMove() utils.Vec2
+	ShoppingList() []Map.Item
+	Cart() map[string]*Map.Item
 	VisionManager() VisionManager
+}
+
+type BaseAgent struct {
+	MovableEntity
+
+	id            AgentID
+	env           *Environment
+	agentBehavior AgentBehavior
+
+	moveChan  chan MoveRequest
+	syncChan  chan int
+	startChan chan StartRequest
+	exitChan  chan ExitRequest
+
+	moveChanResponse  chan bool
+	startChanResponse chan bool
+	exitChanResponse  chan bool
+
+	currentPath              *pathfinding.Path
+	moveTargetX, moveTargetY float64
+	hasDestination           bool
+	hasSpawned               bool
+
+	stuckCounter  int
+	stuckDetector *StuckDetector
+	visionManager *VisionManager
+}
+
+func NewBaseAgent(id string, pos [2]float64, env *Environment, moveChan chan MoveRequest, syncChan chan int, startChan chan StartRequest, exitChan chan ExitRequest) *BaseAgent {
+
+	agent := &BaseAgent{
+		id:  AgentID(id),
+		env: env,
+		MovableEntity: MovableEntity{
+			coordinate:   utils.Vec2{X: pos[0], Y: pos[1]},
+			speed:        constants.BASE_AGENT_SPEED,
+			dx:           0,
+			dy:           0,
+			lastPosition: utils.Vec2{X: pos[0], Y: pos[1]},
+		},
+		moveChan: moveChan,
+		syncChan: syncChan,
+
+		startChanResponse: make(chan bool),
+		exitChanResponse:  make(chan bool),
+
+		moveChanResponse: make(chan bool),
+		hasDestination:   false,
+		hasSpawned:       false,
+		stuckCounter:     0,
+		startChan:        startChan,
+		exitChan:         exitChan,
+	}
+	agent.movementManager = NewMovementManager(agent)
+	agent.stuckDetector = NewStuckDetector(agent)
+	agent.visionManager = NewVisionManager(agent)
+
+	return agent
+}
+
+func (ag *BaseAgent) ID() AgentID {
+	return ag.id
+}
+
+func (ag *BaseAgent) HasSpawned() bool {
+	return ag.hasSpawned
+}
+
+func (ag *BaseAgent) Start() {
+	ag.startChan <- StartRequest{Agt: ag, ResponseChannel: ag.startChanResponse}
+	ag.hasSpawned = <-ag.startChanResponse
+
+	logger.Infof("Agent %s starting at position (%.1f, %.1f)", ag.id, ag.coordinate.X, ag.coordinate.Y)
+
+	go func() {
+		var step int
+		for {
+			select {
+			case <-ag.exitChanResponse:
+				logger.Infof("Agent %s finished", ag.id)
+				return
+			default:
+				step = <-ag.syncChan
+				// perception := <-ag.viewChan
+
+				ag.Percept()
+				ag.Deliberate()
+				ag.Act()
+				step++
+				ag.syncChan <- step
+			}
+		}
+	}()
+}
+
+func (ag *BaseAgent) Percept() {
+	ag.agentBehavior.Percept()
+}
+
+func (ag *BaseAgent) Deliberate() {
+	ag.agentBehavior.Deliberate()
+
+}
+
+func (ag *BaseAgent) Act() {
+	ag.agentBehavior.Act()
+}
+
+func (ag *BaseAgent) GetCurrentPath() *pathfinding.Path {
+	return ag.currentPath
+}
+
+func (ag *BaseAgent) VisionManager() VisionManager {
+	return *ag.visionManager
+}
+
+func (ag *BaseAgent) processPathMovement() {
+	// If path, follow it
+	if ag.currentPath != nil && !ag.currentPath.IsComplete() {
+		ag.movementManager.FollowPath()
+	} else {
+		// Stop movement
+		ag.dx = 0
+		ag.dy = 0
+	}
 }
 
 type BaseAgent struct {
