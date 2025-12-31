5cfc8fbebfe14edb40969008fc5e5d69b95dc8e6
Maël <chanon.mael@gmail.com>
2025-12-31 02:37:05 +0100

    plot aggressiveness + fixes

--- pkg/graphics/game.go
+++ pkg/graphics/game.go
@@ -23,6 +23,7 @@ type Game struct {
 	startMenu                 *Hud.StartMenu
 	inMenu                    bool
 	guardsFOV                 bool
+	ticDuration               int
 }
 
 func NewGame(screenWidth, screenHeight int) *Game {
@@ -36,6 +37,7 @@ func NewGame(screenWidth, screenHeight int) *Game {
 		Hud:           *Hud.NewHud(),
 		inMenu:        true,
 		guardsFOV:     false,
+		ticDuration:   constants.TIC_DURATION,
 	}
 	game.startMenu = Hud.NewStartMenu(func(p Hud.Properties) {
 		game.startSimulation(p)
@@ -44,6 +46,12 @@ func NewGame(screenWidth, screenHeight int) *Game {
 	game.UI = createUI(game)
 
 	return game
+}
+
+func (game *Game) SetTicDuration(duration int) {
+	game.ticDuration = duration
+	game.Simulation.SetTicDuration(duration)
+
 }
 func (game *Game) startSimulation(p Hud.Properties) {
 	mapPath := fmt.Sprintf("%s/%s", constants.MAPS_PATH, p.Filename)
@@ -55,12 +63,16 @@ func (game *Game) startSimulation(p Hud.Properties) {
 
 	mapName := extractMapName(mapPath)
 	shoppingListsPath := fmt.Sprintf("%s/shopping_lists.json", constants.MAPS_PATH)
-	simu := Simulation.NewSimulation(0, constants.TIC_DURATION, mapData, constants.DELTA_TIME, constants.AGENT_SEARCH_RADIUS, mapName, shoppingListsPath)
+	simu := Simulation.NewSimulation(0, game.ticDuration, mapData, constants.DELTA_TIME, constants.AGENT_SEARCH_RADIUS, mapName, shoppingListsPath)
 	logger.Info("Adding agents...")
 	logger.Infof("Creating %d client from config", p.ClientNumber)
 	for i := 1; i <= p.ClientNumber; i++ {
 		agentID := fmt.Sprintf("agent%d", i)
-		simu.AddClient(agentID, rand.Float64()*(1+p.AgentAggressiveness))
+		aggressiveness := 0.0
+		if rand.Float64() > (1.0 - p.AgentAggressiveness) {
+			aggressiveness = 1.0
+		}
+		simu.AddClient(agentID, aggressiveness)
 	}
 	for i := 1; i <= p.GuardNumber; i++ {
 		agentID := fmt.Sprintf("guard%d", i)
